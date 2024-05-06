813c7c02990d10a0e15172f41e1af8c456ea31a8
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2024-05-05 22:48:51 -0500

    Clase05FinalBinding

--- app/src/main/java/com/paredes/mythirdapp/MainActivity.kt
+++ app/src/main/java/com/paredes/mythirdapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.paredes.mythirdapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,33 +13,55 @@ import com.paredes.mythirdapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //var btnLogin=findViewById<Button>(R.id.btn_login)
-
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Sin bindig
+        /* var btnLogin = findViewById<Button>(R.id.btn_login)
+         var etxtUser = findViewById<EditText>(R.id.etxt_user)
+         var etxtPassword = findViewById<EditText>(R.id.etxt_password)
+
+        btnLogin.setOnClickListener {
+            if (etxUser.text.toString() == "admin" &&
+                etxPassword.text.toString() == "admin") {
+//                Toast.makeText(this,
+//                    "Bienvenido", Toast.LENGTH_SHORT)
+//                    .show()
+                Snackbar.make(etxUser,"Bienvenido",Snackbar.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this,
+                    "Hay un error en los datos ingresados", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+*/
+
+        //con bindign
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       /* etxtUser = findViewById<EditText>(R.id.etxt_user)
-
-
-        var btnLogin = findViewById<Button>(R.id.btn_login)
-        var etxtUser = findViewById<EditText>(R.id.etxt_user)
-        var etxtPassword = findViewById<EditText>(R.id.etxt_password)*/
-
-        binding.
-
+        //con binding
         binding.btnLogin.setOnClickListener {
-            if (etxtUser.text.toString() == "admin" && etxtPassword.text.toString() == "admin") {
+            if (binding.etxtUser.text.toString() == "admin" && binding.etxtPassword.text.toString() == "admin") {
                 Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
 
             } else {
                 Toast.makeText(this, "Credenciales incorrectas!", Toast.LENGTH_SHORT).show()
             }
+
         }
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("UCE","Metodo onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("UCE","Metodo onResume")
     }
 }
