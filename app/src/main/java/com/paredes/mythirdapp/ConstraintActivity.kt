9887dbb829f19bdba60bdfcafc8718284d00d355
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2024-05-07 12:12:56 -0500

    LoginBaseFuncionalActivityMainBinding

--- app/src/main/java/com/paredes/mythirdapp/ConstraintActivity.kt
+++ app/src/main/java/com/paredes/mythirdapp/ConstraintActivity.kt
@@ -1,20 +1,32 @@
 package com.paredes.mythirdapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.paredes.mythirdapp.databinding.ActivityConstraintBinding
+import com.paredes.mythirdapp.databinding.ActivityMainBinding
 
 class ConstraintActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConstraintBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_constraint)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityConstraintBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonIngresar.setOnClickListener {
+            if (binding.editTextUser.text.toString() == "user" && binding.editTextPassword.text.toString()=="password") {
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Usuario y/o contraseña mal ingresados", Toast.LENGTH_SHORT).show()
+
+            }
+
         }
+
+
     }
 }
